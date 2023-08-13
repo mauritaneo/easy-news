6f504dbb73b43487e685bf4df9f5f246dcbdb5b9
mauritaneo <aless.bonora@gmail.com>
2023-08-14 01:53:23 +0200

    Netlify connect 4

--- next.config.js
+++ next.config.js
@@ -5,6 +5,22 @@ module.exports = {
   experimental: {
     forceSwcTransforms: false,
   },
+
+  async headers() {
+
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   
   webpack: (config, { isServer }) => {
     // Merge your custom webpack configuration
